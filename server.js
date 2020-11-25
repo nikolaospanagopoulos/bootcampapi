d374d4b28031a49d71304525c37d70aa1b7aa6cc
NikosPanagopoulos <70824044+NikosPanagopoulos@users.noreply.github.com>
2020-11-25 18:43:50 +0200

    express setup

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config({path:"'./config/config.env"})
+
+const app=express()
+
+
+const PORT = process.env.PORT || 5000
+
+    app.listen(PORT, ()=>console.log(`server runs on port ${PORT} in ${process.env.NODE_ENV}`)) 
